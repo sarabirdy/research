824a435c1b60223a04ac9e13c6332f0b7f8b654d
sarabirdy <sarabirdy@gmail.com>
2015-06-22 09:38:15 -0500

    Update obj_detect_nao.py

--- obj_detect_nao.py
+++ obj_detect_nao.py
@@ -11,27 +11,33 @@ from nao_image_class import mySort, NaoImage
 
 IP = "bobby.local"
 PORT = 9559
-CameraID = 0
+CameraID = 1
 
 class naoObjectDetection(NaoImage):
 
 	def __init__(self, IP, PORT, CameraID):
 		NaoImage.__init__(self, IP, PORT, CameraID)
-		self._threshold = self._array
-		self._contours = []
-		self.startTimer(10000)
+		self.startTimer(100)
 
 	def ObjDetect(self):
 		eightbit = QImage.convertToFormat(self._image, QImage.Format_Indexed8)
-		#print eightbit
 		eightbit = qim.byte_view(eightbit)
-		#print eightbit
-		self._threshold = cv2.adaptiveThreshold(eightbit, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 11, 2)
-		self._contours = cv2.findContours(self._threshold, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-		#self._contours = cv2.approxPolyDP(self._contours, .01 * cv2.arcLength(self._contours, True), True)
-		self._array = cv2.drawContours(self._array, [self._contours], 0, (255, 0, 0), 2)
+		eightbit = cv2.medianBlur(eightbit, 19)
+		thr = cv2.adaptiveThreshold(eightbit, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 11, 2)
+		#ret, thr = cv2.threshold(eightbit, 127, 255, cv2.THRESH_BINARY)
+		cv2.imshow("NAO", thr)
+		k = cv2.waitKey(100) & 0xff
+		if k == 27:
+			cv2.destroyAllWindows()
+			sys.exit(app.exec_())
+		contours0, hierarchy = cv2.findContours(thr, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+		print len(contours0)
+		for cnt in contours0[0]:
+			contours = cv2.approxPolyDP(cnt, cv2.arcLength(contours0, True), True)
+			#contours = cv2.approxPolyDP(contours, .01 * cv2.arcLength(contours, True), True)
+			self._array = cv2.drawContours(self._array, [cnt], 0, (255, 0, 0), 2)
 		cv2.imshow("NAO", self._array)
-		k = cv2.waitKey(10000) & 0xff
+		k = cv2.waitKey(100) & 0xff
 		if k == 27:
 			cv2.destroyAllWindows()
 			sys.exit(app.exec_())
